1020a664871edb887001b425f384f73d991b8fcc
Derry Nixon <nixondc93@gmail.com>
2016-10-26 22:27:00 -0700

    finished bottles-of-beer.js and shakespearian-insult.js

--- challenges/bottles-of-beer-song.js
+++ challenges/bottles-of-beer-song.js
@@ -22,3 +22,20 @@
 */
 
 // YOUR CODE HERE
+function bottlesOfBeer(num){
+	song = '';
+
+	for(num; num >= 1; num--){
+		if( num === 1){
+			song += num + ' bottle of beer on the wall,\n' + num + ' bottle of beer! \nTake one down and pass it around,\nNo more bottles of beer on the wall...\n\n';
+		} else if(num === 2){
+			song += num + ' bottles of beer on the wall,\n' + num + ' bottles of beer! \nTake one down and pass it around,\n' + (num - 1) + ' bottle of beer on the wall...\n\n';
+		} else{
+		song += num + ' bottles of beer on the wall,\n' + num + ' bottles of beer! \nTake one down and pass it around,\n' + (num - 1) + ' bottles of beer on the wall...\n\n';
+		}
+
+	}
+	return song;
+}
+
+console.log(bottlesOfBeer(10));
--- challenges/shakespearian-insult-generator.js
+++ challenges/shakespearian-insult-generator.js
@@ -17,3 +17,20 @@ var second_word = ["weather-bitten", "unchin-snouted", "toad-spotted", "tickle-b
 var third_word = ["wagtail", "whey-face", "vassal", "varlet", "strumpet", "skainsmate", "scut", "ratsbane", "pumpion", "puttock", "pignut", "pigeon-egg", "nut-hook", "mumble-news", "moldwarp", "miscreant", "minnow", "measle", "mammet", "malt-worm", "maggot-pie", "lout", "lewdster", "joithead", "hugger-mugger", "horn-beast", "hedge-pig", "harpy", "haggard", "gudgeon", "giglet", "fustilarian", "foot-licker", "flirt-gill", "flax-wench", "flap-dragon", "dewberry", "death-token", "codpiece", "coxcomb", "clotpole", "clack-dish", "canker-blossom", "bum-bailey", "bugbear", "boar-pig", "bladder", "barnacle", "baggage", "apple-john"];
 
 // YOUR CODE HERE
+
+function generateInsults(n = 3, name = null){
+	var insult = '';
+	var wordBank = [first_word, second_word, third_word];
+
+	if(name !== null){
+		insult = "Why Justin, you ";
+	}
+
+	for(var i = 1; i <= n; i++){
+		insult += wordBank[(i%3)][Math.floor(Math.random() * wordBank[i%3].length)] + " ";
+	}
+	return insult;
+}
+
+
+console.log(generateInsults());
